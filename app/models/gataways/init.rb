be70e213f960f353347f7c28ee388f8e57205ee1
will.kuan <will.kuan@iss.nthu.edu.tw>
2018-11-03 19:31:19 +0800

    update

--- app/models/gataways/init.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'lbs_api.rb'
-require_relative 'geo_handler.rb'
--- app/models/mappers/init.rb
+++ app/models/mappers/init.rb
@@ -1,4 +1,3 @@
-
 # frozen_string_literal: true
 
 Dir.glob("#{__dir__}/*.rb").each do |file|

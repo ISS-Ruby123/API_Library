0c9fd966a461ebc822b6f22c3d50ebdf9d5c561a
will.kuan <will.kuan@iss.nthu.edu.tw>
2018-11-03 19:33:24 +0800

    update

--- app/init.rb
+++ app/init.rb
@@ -3,8 +3,4 @@
 folders = %w[models infrastructure controllers]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> ed63c9ba2a357ea38064577d3730356f8ade06cd

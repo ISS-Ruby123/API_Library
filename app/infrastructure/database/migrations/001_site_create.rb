3be8ebaf962f9c7b8c2046f697afcfb5cee27cd9
will.kuan <will.kuan@iss.nthu.edu.tw>
2018-11-11 15:35:53 +0800

    update app/infrastrucure/database/migrations/001_site_create.rb

--- app/infrastructure/database/migrations/001_site_create.rb
+++ app/infrastructure/database/migrations/001_site_create.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 require 'sequel'
+require 'pg'
 
-DB = Sequel.sqlite
-DB.create_table :site do
-  primary_key :geo_code
-  Float     :lat, null: false
-  Float     :lng, null: false
-  String      :name, null: false
-  String      :address, null: false
+Sequel.migration do
+  change do
+    create_table(:sites) do
+      primary_key  :id
+      String       :name, null: false
+      String       :address, null: false
+      Float        :lat, null: false
+      Float        :lng, null: false
+      String       :geocode, unique: true, null: false
+
+      DateTime     :created_date
+      DateTime     :updated_date
+    end
+  end
 end

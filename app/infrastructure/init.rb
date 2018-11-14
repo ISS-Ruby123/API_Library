84d4dd972ca8fd5ca6f0e6c993811e342f54993e
coolly <s107078515@m107.nthu.edu.tw>
2018-11-14 21:31:56 +0800

    update init.rb

--- app/infrastructure/init.rb
+++ app/infrastructure/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-folders = %w[domain infrastructure controllers]
+folders = %w[gateways database]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
